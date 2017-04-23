83f9e51ff74a8e0c2471db9ec6acf2246726d370
Zac Bowen <zbowen93@gmail.com>
2017-04-23 04:53:57 -0500

    Code Cleanup Fixed formatting.

--- src/main/java/org/github/zbb93/FreeHoldEm/Tuple.java
+++ src/main/java/org/github/zbb93/FreeHoldEm/Tuple.java
@@ -1,12 +1,12 @@
 package org.github.zbb93.FreeHoldEm;
 public class Tuple {
 
-    public final int x;
-    public final int y;
+	public final int x;
+	public final int y;
 
-    public Tuple(int x, int y) {
+	public Tuple(int x, int y) {
 
-	this.x = x;
-	this.y = y;
-    }
+		this.x = x;
+		this.y = y;
+	}
 }
