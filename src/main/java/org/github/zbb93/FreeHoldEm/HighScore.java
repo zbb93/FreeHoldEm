76465de86fda33fb79f874fa619b35199c4351dc
Zac Bowen <zbowen93@gmail.com>
2017-04-23 04:48:07 -0500

    Code Cleanup Added license and removed usages of 'this' where unnecessary. Removed non-source files.

--- src/main/java/org/github/zbb93/FreeHoldEm/HighScore.java
+++ src/main/java/org/github/zbb93/FreeHoldEm/HighScore.java
@@ -1,5 +1,21 @@
+/*
+ * FreeHoldEm
+ * Copyright 2015-2017 by Zachary Bowen
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package org.github.zbb93.FreeHoldEm;
-//So we can sort them in arrayList
+
 public class HighScore {
 	private final String name;
 	private final int score;
@@ -11,11 +27,11 @@ public class HighScore {
 	}
 	
 	public String getName() {
-		return this.name;
+		return name;
 	}
 	
 	public int getScore() {
-		return this.score;
+		return score;
 	}
 	
 	/**
